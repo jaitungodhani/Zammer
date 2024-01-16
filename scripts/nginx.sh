210a833172dbc88b7701115da8cdfba98d943413
jaitunservicepack <jaitun.godhani@servicepack.ai>
2024-01-16 21:55:54 +0530

    add cicd pipeline files

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,8 +4,8 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/Zammer/nginx/nginx.conf /etc/nginx/sites-available/blog
-sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
+sudo cp /home/ubuntu/Zammer/nginx/nginx.conf /etc/nginx/sites-available/zammer
+sudo ln -s /etc/nginx/sites-available/zammer /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
